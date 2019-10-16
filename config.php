a7c4b6033fc36b3ea180a963b74a8826d4cfdb72
Emanuele Munafò <ema.muna95@gmail.com>
2019-10-16 19:12:31 +0200

    Added configuration file

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+
+/*This is a PHP Config file */
+define('DBName', 'softeng2');
+define('DBUser', 'softeng2');
+define('DBPassword', '3;ftBE>72h');
+define('DBAddr', 'localhost');
+define('TITLE', 'Softeng 2 project 1');
+define('PLATFORM_PATH', 'http://softeng2.my.to/');
+//define('FULL_PATH', $_SERVER['DOCUMENT_ROOT'].'/');
+
+?>
