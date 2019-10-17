8973a29a4196aaf0ed17d968bd07a9de4a218193
Emanuele Munafò <ema.muna95@gmail.com>
2019-10-17 11:03:49 +0200

    Fixed export of DB: just a table was exported

--- dbsetup.sql
+++ dbsetup.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Creato il: Ott 16, 2019 alle 23:45
+-- Creato il: Ott 17, 2019 alle 11:02
 -- Versione del server: 5.7.27-0ubuntu0.16.04.1
 -- Versione PHP: 7.0.33-0ubuntu0.16.04.6
 
@@ -43,6 +43,49 @@ INSERT INTO `Authentication` (`FrontOffice`, `Password`, `Permission`, `Counter`
 ('clerk', '$2y$12$hqVeuuk9y1B2sB/zISy6Kuxo1XSBPAjxXbQsQKbKjdytpcmSWjwZa', 'Clerk', 0, 0),
 ('test', '$2y$12$zRXRfWvG3pdlrZukzYRGm.bxhxzrmP/DwneGPIGw7Bdgv6x0NnXtW', 'Clerk', 0, 1);
 
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `Queue`
+--
+
+CREATE TABLE `Queue` (
+  `ServiceID` int(11) NOT NULL,
+  `TicketNumber` int(11) NOT NULL,
+  `Timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dump dei dati per la tabella `Queue`
+--
+
+INSERT INTO `Queue` (`ServiceID`, `TicketNumber`, `Timestamp`) VALUES
+(1, 0, '2019-10-16 19:51:59'),
+(2, 1, '2019-10-16 19:51:59'),
+(2, 0, '2019-10-16 19:51:59'),
+(1, 1, '2019-10-16 19:51:59'),
+(1, 2, '2019-10-16 19:51:59');
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `Service`
+--
+
+CREATE TABLE `Service` (
+  `Name` varchar(65) NOT NULL,
+  `ID` int(11) NOT NULL,
+  `Counter` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dump dei dati per la tabella `Service`
+--
+
+INSERT INTO `Service` (`Name`, `ID`, `Counter`) VALUES
+('GetParcel', 1, 0),
+('SendParcel', 2, 0);
+
 --
 -- Indici per le tabelle scaricate
 --
@@ -53,6 +96,21 @@ INSERT INTO `Authentication` (`FrontOffice`, `Password`, `Permission`, `Counter`
 ALTER TABLE `Authentication`
   ADD PRIMARY KEY (`FrontOffice`);
 
+--
+-- Indici per le tabelle `Service`
+--
+ALTER TABLE `Service`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `Service`
+--
+ALTER TABLE `Service`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
