c62060be751ec629696a2995317f2ecb9d163bbe
Emanuele Munafò <ema.muna95@gmail.com>
2019-10-17 19:06:30 +0200

    Refactoring (removed frontend sample unused code)

--- ticketDispatcher.php
+++ ticketDispatcher.php
@@ -4,12 +4,8 @@ require_once("functions.php");
 
 if($_GET['action'] == "generateTicket" && isset($_POST['service'])){
     // Call generate ticket function
-    //$ticketinfo = get_a_new_ticket($serviceID (or $serviceName))
-    // Following code is just to let the frontend work for now: TODO: remove when ready token generation
-    $ticket_info["ticketN"] = 10;
-    $ticket_info["service"] = "GUITest";
-    // Easy way to avoid query
     $ticket_info = add_top($_POST['service']);
+    // Easy way to avoid query
     $ticket_info["service"] = $_POST['service']; 
 
     //print_r($ticket_info);//Array ( [ID] => 10 [serviceID] => 1 [ticketN] => 7 [timestamp] => 2019-10-17 18:39:29 )
