68e01380b465dd3c29bdcb046b9ebcd1b884b462
Riccardo Mamone <s265425@studenti.polito.it>
2019-10-17 10:57:30 +0200

    first draft frontend

---
+++ footer.php
@@ -0,0 +1,13 @@
+<?php
+
+?>
+<!-- Footer -->
+<footer class="page-footer font-small blue fixed-bottom">
+  <!-- Copyright notice! -->
+  <div class="footer-copyright text-center py-3">© 2019 TeamJ Copyright:
+    <a href="#">softeng2.my.to</a>
+  </div>
+</footer>
+<!-- Footer -->
+</body>
+</html>
